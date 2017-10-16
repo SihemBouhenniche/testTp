57eaf82c23f8f5bdc246f66b644cf01f5be1dfa2
SihemBouhenniche <fs_bouhenniche@esi.dz>
2017-10-16 22:54:31 +0100

    Creation de la classe 'VectorHelper' avec la déclaration des attributs et les constructeurs.

---
+++ src/testtp/VectorHelper.java
@@ -0,0 +1,32 @@
+/*
+ * TP 1 IGL sur l'utilisation des outils de gestion des projets 'github , javadoc et
+ *  le tests unitaires| class VectorHelper
+ * 
+ */
+package testtp;
+
+/**
+ *
+ * @author SihemBouhennoche
+ */
+public class VectorHelper {
+    //attribut 1 : taille du tableau
+    private int taille;
+    //attrinut 2 : les elements du tableau
+    private int[] tabElement;
+
+    //le constructeur par deafut
+    public VectorHelper() {
+    }
+    
+    //le constructeur avec paramétres
+    public VectorHelper(int taille, int[] tabElement) {
+        this.taille = taille;
+        this.tabElement = tabElement;
+    }
+
+   
+  
+    
+    
+}
